0010d958bd7843e10a3f7d57081ad563a2aeb05c
Alekoff <borenko.oleg@gmail.com>
2017-10-18 23:49:04 +0300

    csgoskins

--- admin/components/adm_foot.php
+++
@@ -1,8 +0,0 @@
-
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="<? echo site_url('/assets/js/bootstrap.min.js'); ?>"></script>
-
-    <!-- Enable responsive features in IE8 with Respond.js (https://github.com/scottjehl/Respond) -->
-    <script src="<? echo site_url('/assets/js/respond.js'); ?>"></script>
-  </body>
-</html>
